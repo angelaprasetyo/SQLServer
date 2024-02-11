7659296235817a981f382e31568bfd3219930019
angelaprasetyo14 <angelaprasetyo875@gmail.com>
2024-02-11 13:05:49 +0700

    first commit

--- Select+From.sql
+++ Select+From.sql
@@ -3,34 +3,34 @@
  *, Top, District, Count, As, Max, Min, Avg
  */
 
- --SELECT TOP 5 *
- --FROM EmployeeDemographics
+ SELECT TOP 5 *
+ FROM EmployeeDemographics
  //*Memilih data 5 teratas*//
 
- --SELECT DISTINCT(Gender)
- --FROM EmployeeDemographics
+ SELECT DISTINCT(Gender)
+ FROM EmployeeDemographics
   //*Memilih kolom tertentu*//
 
- --SELECT COUNT(LastName) AS Last_Name_Count
- --FROM EmployeeDemographics
+ SELECT COUNT(LastName) AS Last_Name_Count
+ FROM EmployeeDemographics
   //*Menghitung jumlah data pada lastname*//
 
- --SELECT *
- --FROM EmployeeSalary
+ SELECT *
+ FROM EmployeeSalary
   //*Menampilkan keseluruhan data dari data yang dimaksud*//
 
- --SELECT MIN (Salary) As Min_Salary
- --FROM EmployeeSalary
+ SELECT MIN (Salary) As Min_Salary
+ FROM EmployeeSalary
   //*Menampilkan Paling Kecil*//
 
- --SELECT MAX (Salary) As Max_Salary
- --FROM EmployeeSalary
+ SELECT MAX (Salary) As Max_Salary
+ FROM EmployeeSalary
   //*Menampilkan Paling Besar*//
 
- --SELECT AVG (Salary) As AVG_Salary
- --From EmployeeSalary
+ SELECT AVG (Salary) As AVG_Salary
+ From EmployeeSalary
   //*Menampilkan Tengah -Tengah*//
 
- --SELECT *
- --FROM SQLTutorial.dbo.EmployeeSalary
+ SELECT *
+ FROM SQLTutorial.dbo.EmployeeSalary
   //*Menampilkan Data Keseluruhan dari database*//
